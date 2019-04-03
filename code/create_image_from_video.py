132e7fba6d3dfd32320720e64ac969af814c32ea
Coach <laurentcesaro17@gmail.com>
2019-04-03 15:26:13 +0200

    Update create_image_from_video.py

--- code/create_image_from_video.py
+++ code/create_image_from_video.py
@@ -1,31 +1,35 @@
-#Autor : Laurent Cesaro
+"""
+@author: Laurent Cesaro
 
+1 - Convert a video from youtube to mp4
+"""
+#https://www.youtube.com/watch?v=r2ShaMdKF6E
 import cv2
-import numpy as np
-import os
 
-# Playing video from file:
-cap = cv2.VideoCapture('video.mp4')
+    
+# Video PATH
+videoFile = "videoplayback.mp4"
+# Read Video
+vidcap = cv2.VideoCapture(videoFile)
+success,image = vidcap.read()
 
-try:
-    if not os.path.exists('data'):
-        os.makedirs('data')
-except OSError:
-    print ('Error: Creating directory of data')
+#################### Setting up parameters ################
 
-currentFrame = 0
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+seconds = 1
+fps = vidcap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
+multiplier = fps * seconds
 
-    # Saves image of the current frame in jpg file
-    name = './data/frame' + str(currentFrame) + '.jpg'
-    print ('Creating...' + name)
-    cv2.imwrite(name, frame)
+#################### Initiate Process ################
 
-    # To stop duplicate images
-    currentFrame += 1
+while success:
+    frameId = int(round(vidcap.get(1))) #current frame number, rounded b/c sometimes you get frame intervals which aren't integers...this adds a little imprecision but is likely good enough
+    success, image = vidcap.read()
+    
+    if frameId % multiplier == 0:
+        # Resize Image
+        image = cv2.resize(image, (300, 300))
+        # Save Image - Define path
+        cv2.imwrite("screen/%d.png" % frameId, image)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+vidcap.release()
+print("Complete")
