082bfcf721f748e711ac4b87ba912cbe375b59a6
Coach <laurentcesaro17@gmail.com>
2019-06-29 14:55:28 +0200

    Update boat_detection_opencv2.py

--- code/boat_detection_opencv2.py
+++ code/boat_detection_opencv2.py
@@ -18,6 +18,8 @@ import json
 
 # command curl prediction
 curl_cmd = "curl -X POST http://127.0.0.1:8080/predictions/ssd -T /path/mxnet-model-server/docs/images/prediction.png"
+# font text
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 x = 600
 y = 600
@@ -31,7 +33,7 @@ while(True):
     
     # save img to png for prediction
     img = Image.fromarray(screen)   
-    img.save('docs/images/prediction.png') 
+    img.save('/path/mxnet-model-server/docs/images/prediction.png') 
     
     # Make predictions
     p = Popen(curl_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
@@ -45,9 +47,10 @@ while(True):
         continue
     else:
         img = cv2.imread("/path/mxnet-model-server/docs/images/prediction.png",0)
+        nb_boat = len(bbox)
         for i in range(0, len(bbox)):
             cv2.rectangle(img,(bbox[i][1],bbox[i][2]),(bbox[i][3],bbox[i][4]),(255,255,255),3)
-        
+            cv2.putText(img, "Nombre de bateaux : "+str(nb_boat),(10,10), font, 0.3,(255,255,255),1,cv2.LINE_AA)
     cv2.imshow("image", img)
     last_time = time.time()
     time.sleep(5)
